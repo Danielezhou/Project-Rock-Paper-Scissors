1f5d8d45466d1086bedfeecd6cd4737faa916c17
Bijinesman <daniele.zhou1996@gmail.com>
2022-11-30 00:42:52 +0100

    add first function getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 /* Rock Paper Scissors game Pseudocode
+
 1) getComputerChoice function to activate computer
 2) Write a function with 2 parameters (playerSelection,computerSelection)
 return a string to declare winner "You lose!" || "you lost"
@@ -7,3 +8,9 @@ return a string to declare winner "You lose!" || "you lost"
 4) write a NEW function called game().
 5)call playround function inside of game() to play a 5 round game with LOOPS
 */
+
+function getComputerChoice () {
+    let choice = ["Rock", "Paper", "Scissors"]
+    return choice[Math.floor(Math.random()*choice.length)];
+
+}
