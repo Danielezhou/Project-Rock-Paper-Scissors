6ef0497476e6afdecf39f40de0a25d7d97d1952a
Bijinesman <daniele.zhou1996@gmail.com>
2022-11-30 00:31:17 +0100

    Write Pseudocode

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/* Rock Paper Scissors game Pseudocode
+1) getComputerChoice function to activate computer
+2) Write a function with 2 parameters (playerSelection,computerSelection)
+return a string to declare winner "You lose!" || "you lost"
+    •make playerSelection case-insensitive!
+3)return the results of this function call.
+4) write a NEW function called game().
+5)call playround function inside of game() to play a 5 round game with LOOPS
+*/
