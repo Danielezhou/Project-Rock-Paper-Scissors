f81350c329580671acd1c76ca2c6b2b73505c522
Bijinesman <daniele.zhou1996@gmail.com>
2022-12-01 23:25:48 +0100

    implement last function game()

--- javascript.js
+++ javascript.js
@@ -9,37 +9,51 @@ return a string to declare winner "You lose!" || "you lost"
 5)call playround function inside of game() to play a 5 round game with LOOPS
 */
 
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+let countPlayer = 0;
+let countComputer = 0;
+
 function getComputerChoice () {
     let choice = ["rock", "paper", "scissors"]
     return choice[Math.floor(Math.random()*choice.length)];
 }
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
 
 function round (playerSelection, computerSelection){
     computerSelection = getComputerChoice();
     playerSelection = prompt("rock, paper, scissors").toLowerCase();
     if (playerSelection === computerSelection){
         alert ("It's a tie");
-        return "It's a tie";
+        return 3;
     }
     else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock"){
-        alert ("You Lose!");
-        return "You Lose";
+        alert ("You Lose this round!");
+        countComputer++;
+        return 1;
     }
     else if(playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
-        alert("You Win!");
-        return ;
+        alert("You Win this round!");
+        countPlayer++;
+        return 2;
     }
     else {
         alert(Error);
         return -1;
     }
-}
 
+}
 function game() {
     for (let i = 0; i < 5; i++){
         round(playerSelection,computerSelection);
+    }
+    if (countPlayer > countComputer){
+        alert("You Win")
+    }
+    else if (countComputer > countPlayer){
+        alert("You Lose")
+    }
+    else{
+        alert("Tie")
     }
 }
 
